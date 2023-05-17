5fd41f0d46a022dddfaee838daf3564b162f07a7
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-05-17 10:43:21 +0000

    Link to Github pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Intl Man of Mystery - understanding the Intl object
+[See the examples here](https://sebug.github.io/intl-man-of-mystery/)
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const frChDateTimeFormat = Intl.DateTimeFormat('fr-CH');
+const frChDateTimeFormat = Intl.DateTimeFormat('fr-CH', {
+    dateStyle: 'full',
+    timeStyle: 'full'
+});
 
 const currentdatetimefrch = document.querySelector('#currentdatetimefrch');
 currentdatetimefrch.innerText = frChDateTimeFormat.format(new Date());
