a6960d21a092e189ddc34c23672e631a7145463b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-05-17 10:48:28 +0000

    Supported locales case

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const frChDateTimeFormat = Intl.DateTimeFormat('fr-CH', {
-    dateStyle: 'full',
-    timeStyle: 'full'
+    dateStyle: 'medium',
+    timeStyle: 'short'
 });
 
 const currentdatetimefrch = document.querySelector('#currentdatetimefrch');
 currentdatetimefrch.innerText = frChDateTimeFormat.format(new Date());
+
+const supportedlocales = document.querySelector('#supportedlocales');
+supportedlocales.innerText = Intl.DateTimeFormat.supportedLocalesOf(['fr-CH', 'en-US', 'fr-ES']).join(', ');
